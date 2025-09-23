17b85339ef0a2b771b8fca3d9dc8011e487db1eb
MrBierflasche <schmidtmoritz143@gmail.com>
2025-09-23 11:22:10 +0200

    1.6

--- Vorkurs/hello.py
+++ Vorkurs/hello.py
@@ -1,2 +1,6 @@
-print("hello world")
-print("123")
+import math
+eingabe = float(input ("Geben Sie eine Zahl ein"))
+
+for i in range(1, 11):
+    print(f"{i} * {eingabe} = {(eingabe * i) / (11 - i)} * {11-i}")
+
