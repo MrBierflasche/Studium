9f5b0baec0a3da0a5f13968a046bbe0f5c519f95
MrBierflasche <schmidtmoritz143@gmail.com>
2025-09-24 09:35:35 +0200

    5.1

--- Vorkurs/hello.py
+++ Vorkurs/hello.py
@@ -1,13 +1,34 @@
 import time
-start = time.time()
-outputs = list(range(1,21 ))
-removeList = []
 
-for i in outputs:
-    if i % 3 == 0:
-        removeList.append( i -1)
-for i in removeList:
-    outputs.remove(i +1 )
-    outputs.insert(i, "*")
-end = time.time()
-print(end - start)
+def eingabe() :
+    return int(input("Bitte Zahl eingeben! "))
+
+def größte(zahlen):
+    rückgabe = zahlen[0]
+    for i in zahlen:
+        if i > rückgabe:
+            rückgabe = i
+            
+    print (rückgabe)
+
+def kleinste(zahlen):
+    rückgabe = zahlen[0]
+    for i in zahlen:
+        if i < rückgabe:
+            rückgabe = i
+            
+    print (rückgabe)
+def main():
+    
+    zahlen = [] 
+    for i in range(0,3):
+        zahlen.append(eingabe())
+    start = time.time()
+    größte(zahlen)
+    kleinste(zahlen)
+    end = time.time()
+    print(end - start)
+
+if __name__ == "__main__":
+    main()
+
