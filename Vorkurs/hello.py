aff996c244a55f82c80cab99fba40add79271070
MrBierflasche <schmidtmoritz143@gmail.com>
2025-09-24 09:20:01 +0200

    4.8

--- Vorkurs/hello.py
+++ Vorkurs/hello.py
@@ -1,15 +1,13 @@
-import math
-outputs = []
-counter = 0
-stopp = input("Bitte geben Sie ein Wort ein. ")
-while ( stopp != "stopp"):
-    outputs.append( stopp )
-    counter = counter +1 
-    print( f"Es wurde {counter} nicht Stopp ausgegeben.")
-    for i in outputs:
-         print(i)
-            
-    stopp = input("Bitte geben Sie ein Wort ein. ")
-    
+import time
+start = time.time()
+outputs = list(range(1,21 ))
+removeList = []
 
-
+for i in outputs:
+    if i % 3 == 0:
+        removeList.append( i -1)
+for i in removeList:
+    outputs.remove(i +1 )
+    outputs.insert(i, "*")
+end = time.time()
+print(end - start)
