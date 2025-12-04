d28b5327f8cab770847475fa87986b2004d3df21
MrBierflasche <schmidtmoritz143@gmail.com>
2025-12-04 12:51:39 +0100

    recursion 2

--- 1_Semester/Programming1/Programming1/src/basic/RecursionTest.java
+++ 1_Semester/Programming1/Programming1/src/basic/RecursionTest.java
@@ -3,8 +3,7 @@ package basic;
 public class RecursionTest {
 
     static void main() {
-        int [] arr = {5, 2, 36, 1};
-        System.out.println(max(arr, 0));
+        System.out.println(isPalindromeRecursive("ABCBA"));
     }
 
     public static int max(int [] arr, int pos){
@@ -14,5 +13,31 @@ public class RecursionTest {
         int maxRest = max(arr, pos + 1);
 
         return (arr[pos] > maxRest) ? arr[pos] : maxRest;
+    }
+
+    // Schreiben Sie einen Palindorm-Test?
+    // Beispiel: isPalindrome("ABBA") == true
+
+    public static boolean isPalindrome(String eingabe){
+        eingabe = eingabe.toLowerCase().replaceAll(" ", "");
+        for (int i = eingabe.length() -1 , j = 0; i >= 0; i--,  j++) {
+            if (eingabe.charAt(i) != eingabe.charAt(j)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isPalindromeRecursive(String eingabe){
+        if (eingabe.length() <= 1){
+            return true;
+        }
+
+        eingabe = eingabe.toLowerCase().replaceAll(" ", "");
+        if (eingabe.charAt(0) != eingabe.charAt(eingabe.length() -1)){
+            return false;
+        }
+
+        return isPalindromeRecursive(eingabe.substring(1, eingabe.length() - 1));
     }
 }
